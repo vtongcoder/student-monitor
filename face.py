b1118712eed0790c2d203ca3074e63ae81190544
Danniel <quangdang12101@gmail.com>
2018-09-08 11:40:50 +0700

    Can detect absolute path and my img

--- face.py
+++ face.py
@@ -1,4 +1,8 @@
 import cv2 #Import opencv
 import os
-
-
+import sys
+image = sys.argv[1]
+face_db = os.path.abspath("face_detect.xml")
+imagePath = os.path.abspath(image)
+print(imagePath)
+print(face_db)
