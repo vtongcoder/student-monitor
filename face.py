66729bc4edc04fc158b23fea103064ea351d9ac6
Danniel <quangdang12101@gmail.com>
2018-09-08 11:41:45 +0700

    Change the face_db name

--- face.py
+++ face.py
@@ -2,7 +2,5 @@ import cv2 #Import opencv
 import os
 import sys
 image = sys.argv[1]
-face_db = os.path.abspath("face_detect.xml")
+face = os.path.abspath("face_detect.xml")
 imagePath = os.path.abspath(image)
-print(imagePath)
-print(face_db)
