b6586d5ba1cdb814f8747bc22714832707f5d29c
Danniel <quangdang12101@gmail.com>
2018-09-09 11:17:48 +0700

    Add some img from famous people (i dun know who they are )

--- train.py
+++ train.py
@@ -2,4 +2,10 @@ import os
 #The base dir
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 #The image dir
-img_dir = os.path.join(BASE_DIR)
+img_dir = os.path.join(BASE_DIR, "image")
+
+for root, dirs, files in os.walk(img_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            print(path)
